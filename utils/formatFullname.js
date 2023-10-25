5f3b1a7c0856d638e064b2cf74eb8aa7461cace1
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-10-25 13:12:40 +0200

    Add tests for fullName (formatting)

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,7 +1,7 @@
 module.exports = (fullName) => {
   if (typeof fullName !== 'string') return 'Error';
   if (fullName.length <= 0) return 'Error';
-  const [ firstName, lastName ] = fullName.split(' ');
-  if(!firstName || !lastName) return false;
-  return firstName[0].toUpperCase + firstName.slice(1).toLowerCase() + lastName[0].toUpperCase + lastName.slice(1).toLowerCase() ;
+  const [firstName, lastName] = fullName.split(' ');
+  if (!firstName || !lastName) return false;
+  return firstName[0].toUpperCase() + firstName.slice(1).toLowerCase() + ' ' + lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
 };
